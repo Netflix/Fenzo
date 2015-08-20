b96d50478a32731daa30de95f101c959e6650597
Satyajit Thadeshwar <sthadeshwar@netflix.com>
2015-08-20 14:26:58 -0700

    Added @JsonCreator annotation to IntervalTrigger constructor

--- fenzo-triggers/src/main/java/com/netflix/fenzo/triggers/IntervalTrigger.java
+++ fenzo-triggers/src/main/java/com/netflix/fenzo/triggers/IntervalTrigger.java
@@ -15,6 +15,7 @@
  */
 package com.netflix.fenzo.triggers;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.joda.time.Interval;
 import org.quartz.ScheduleBuilder;
@@ -24,20 +25,31 @@ import org.quartz.spi.MutableTrigger;
 import rx.functions.Action1;
 
 public class IntervalTrigger<T> extends ScheduledTrigger<T> {
-    final private long repeatInterval;
-    final private int repeatCount;
 
-    public IntervalTrigger(
-            @JsonProperty("iso8601Interval") String interval,
-            @JsonProperty("repeatCount") int repeatCount,
-            @JsonProperty("name") String name,
-            @JsonProperty("data") T data,
-            @JsonProperty("dataType") Class<T> dataType,
-            @JsonProperty("action") Class<? extends Action1<T>> action) {
-        super(name, data, dataType, action, Interval.parse(interval).getStart().toDate(), null);
-        final Interval jodaInterval = Interval.parse(interval);
-        this.repeatCount = repeatCount; // -1 = indefinitely repeat
-        repeatInterval = Interval.parse(interval).getEndMillis() - jodaInterval.getStartMillis();
+    private final long repeatInterval;
+    private final int repeatCount;
+
+    /**
+     * Creates an interval based trigger
+     * @param iso8601Interval - See https://en.wikipedia.org/wiki/ISO_8601 for how to specify intervals with start time/end
+     *                        time/interval
+     * @param repeatCount - repeat count after first trigger. Specify -1 to repeat indefinitely
+     * @param name
+     * @param data
+     * @param dataType
+     * @param action
+     */
+    @JsonCreator
+    public IntervalTrigger(@JsonProperty("iso8601Interval") String iso8601Interval,
+                           @JsonProperty("repeatCount") int repeatCount,
+                           @JsonProperty("name") String name,
+                           @JsonProperty("data") T data,
+                           @JsonProperty("dataType") Class<T> dataType,
+                           @JsonProperty("action") Class<? extends Action1<T>> action) {
+        super(name, data, dataType, action, Interval.parse(iso8601Interval).getStart().toDate(), null);
+        final Interval jodaInterval = Interval.parse(iso8601Interval);
+        this.repeatCount = repeatCount; // -1 is repeat indefinitely
+        this.repeatInterval = Interval.parse(iso8601Interval).getEndMillis() - jodaInterval.getStartMillis();
     }
 
     @Override
