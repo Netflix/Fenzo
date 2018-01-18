00c8eb7f7846eca62b4a2d3a15eb4a1307a90cf2
tbak <tbak@netflix.com>
2018-01-18 11:54:59 -0800

    Add pluggable ENI fitness evaluator, cont (#170)

--- fenzo-core/src/main/java/com/netflix/fenzo/DefaultPreferentialNamedConsumableResourceEvaluator.java
+++ fenzo-core/src/main/java/com/netflix/fenzo/DefaultPreferentialNamedConsumableResourceEvaluator.java
@@ -8,13 +8,13 @@ class DefaultPreferentialNamedConsumableResourceEvaluator implements Preferentia
     static final PreferentialNamedConsumableResourceEvaluator INSTANCE = new DefaultPreferentialNamedConsumableResourceEvaluator();
 
     @Override
-    public double evaluateIdle(int index, double subResourcesNeeded, double subResourcesLimit) {
+    public double evaluateIdle(String hostname, String resourceName, int index, double subResourcesNeeded, double subResourcesLimit) {
         // unassigned: 0.0 indicates no fitness, so return 0.5, which is less than the case of assigned with 0 sub-resources
         return 0.5 / (subResourcesLimit + 1);
     }
 
     @Override
-    public double evaluate(int index, double subResourcesNeeded, double subResourcesUsed, double subResourcesLimit) {
+    public double evaluate(String hostname, String resourceName, int index, double subResourcesNeeded, double subResourcesUsed, double subResourcesLimit) {
         return Math.min(1.0, (subResourcesUsed + subResourcesNeeded + 1.0) / (subResourcesLimit + 1));
     }
 }
